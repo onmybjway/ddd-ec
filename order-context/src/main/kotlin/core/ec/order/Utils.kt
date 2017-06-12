3befafe7a5adcd5aa958faa9755caec2e5db6b82
Your Name <you@example.com>
2017-06-12 10:21:00 +0800

    add net address field for order

--- order-context/src/main/kotlin/core/ec/order/Utils.kt
+++ order-context/src/main/kotlin/core/ec/order/Utils.kt
@@ -3,7 +3,14 @@ package core.ec.order
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.modelmapper.ModelMapper
+import javax.servlet.http.HttpServletRequest
 
+/*
+for JSON
+ */
 val objectMapper = ObjectMapper().registerModule(KotlinModule())
 
 val modelMapper: ModelMapper = ModelMapper()
+
+fun HttpServletRequest.getNetAddress(): String =
+        if (this.getHeader("X-FORWARDED-FOR").isNullOrBlank()) this.remoteAddr else this.getHeader("X-FORWARDED-FOR")
